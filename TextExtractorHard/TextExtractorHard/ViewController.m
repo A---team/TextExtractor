c2289d280f3261c0bd9b3a8ee1dec4467e1eb567
I-Hara <i.hara.224@gmail.com>
2014-05-15 00:21:46 +0900

    [#20] Xcodeプロジェクトを、新規作成。

---
+++ TextExtractorHard/TextExtractorHard/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextExtractorHard
+//
+//  Created by Isao HARA on 2014/05/15.
+//  Copyright (c) 2014年 NESW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
